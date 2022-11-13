9d0fe43b207b317308a3405a619a0e9810203f58
kushandesilva59 <kushandesilva801@gmail.com>
2022-11-13 07:47:21 +0530

    updated

--- asserts/script/game.js
+++ asserts/script/game.js
@@ -1,8 +1,17 @@
+//board
 var blockSize = 25;
 var rows = 20;
 var cols = 20;
 var board ;
 var context ;
+
+//snake head
+var snakeX = blockSize * 5;
+var snakeY = blockSize * 5;
+
+//food
+var foodX = blockSize * 10;
+var foodY = blockSize * 10;
 
 
 window.onload = function () {
@@ -17,4 +26,14 @@ window.onload = function () {
 function update() {
     context.fillStyle = "black";
     context.fillRect(0,0,board.width,board.height);
+
+    context.fillStyle = "yellow";
+    context.fillRect(snakeX,snakeY,blockSize,blockSize);
+
+    context.fillStyle = "red";
+    context.fillRect(foodX,foodY,blockSize,blockSize);
+}
+
+function placeFood(){
+
 }
